10f4894ddb1288532cc36cb71482d64e7a3131da
Brendan Martin <b.t.martin@protonmail.com>
2024-08-07 17:16:48 +0100

    Code to do static factor PCA backtesting

--- fnirvar/modeling/train.py
+++ fnirvar/modeling/train.py
@@ -1,30 +1,122 @@
-from pathlib import Path
+""" 
+Script defining classes and functions to do estimation abd prediction of a factor + NIRVAR model.
+"""
 
-import typer
-from loguru import logger
-from tqdm import tqdm
+#!/usr/bin/env python3 
+# USAGE: ./train_model.py 
 
-from fnirvar.config import MODELS_DIR, PROCESSED_DATA_DIR
+import numpy as np
 
-app = typer.Typer()
+def eigenvalue_ratio():
+    """
+    Function to calculate the number of factors using the eigenvalue ratio method (Horenstein, 2013).
+    """
+    pass
+
+def PCp_criteria():
+    """
+    Function to calculate the number of factors using the PCp criteria method (Bai & Ng, 2002).
+    """
+    pass
+
+def PCp2_criteria(): 
+    """
+    Function to calculate the number of factors using the PCp2 criteria method (Bai & Ng, 2002).
+    """
+    pass
+
+class FactorAdjustment():
+    """
+    Class to estimate the factors and loadings of the common component.
+    Current estimation methods include:
+    - Principal Component Analysis (Stock & Watson, 2002) for the static factor model.
+
+    """
+    def __init__(self,
+                X : np.ndarray,
+                r : int, 
+                lF : int
+                ) -> None:
+        """
+        :param X: Design matrix of shaoe (T, N) 
+        :type X: numpy.ndarray 
+
+        :param r: Number of factors 
+        :type r: int 
+
+        :param lF: Number of lags in the model for the factors 
+        :type lF: int  
+        """
+        self.X = X 
+        self.r = r 
+        self.lF = lF
+
+    @property
+    def N(self):
+        N = self.X.shape[1] 
+        return N
+    
+    @property
+    def T(self):
+        T = self.X.shape[0] 
+        return T
+    
+    @property
+    def T_lF(self):
+        T_lF = self.T - self.lF 
+        return T_lF
 
 
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROCESSED_DATA_DIR / "features.csv",
-    labels_path: Path = PROCESSED_DATA_DIR / "labels.csv",
-    model_path: Path = MODELS_DIR / "model.pkl",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Training some model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Modeling training complete.")
-    # -----------------------------------------
 
+    def loadings(self):
+        """
+        Function to estimate the loadings of the common component.
+        """
+        evecs = np.linalg.eigh(self.X.T @ self.X)[1][:, -self.r:] # Eigenvectors of the covariance matrix corresponding to the r largest eigenvalues 
+        return evecs 
+    
+    def static_factors(self):  
+        """
+        Function to estimate the factors of the common component.
+        """
+        loadings = self.loadings()
+        factors = self.X @ loadings / self.N 
+        return factors 
+    
+    def factor_linear_model(self):
+        """
+        Function to estimate the coefficients of the linear model for the latent factors using OLS.
+        """
+        factors = self.static_factors()
+        targets = factors[self.lF:, :] 
 
-if __name__ == "__main__":
-    app()
+        factor_design = np.zeros((self.T_lF, int(self.r * self.lF)))
+        for tau in range(self.T_lF):
+            F_flat_tau = factors[tau:tau+self.lF, :].flatten() 
+            factor_design[tau, :] = F_flat_tau
+
+        factor_coefficients = np.zeros((self.r, self.r * self.lF))
+        for f in range(self.r):
+            factor_coefficients[f,:] = np.linalg.inv(factor_design.T @ factor_design) @ factor_design.T @ targets[:,f] # OLS estimation 
+        return factor_coefficients
+    
+    def get_idiosyncratic_component(self):
+        """
+        Function to estimate the idiosyncratic component.
+        """
+        xi = self.X - self.static_factors() @ self.loadings().T
+        return xi  
+    
+    def predict_common_component(self):
+        """
+        Function to predict then next day common component.
+        """
+        loadings = self.loadings()
+        factors = self.static_factors()
+        P_hat = self.factor_linear_model() 
+        G_hat_t = factors[-self.lF:, :].flatten()[:, np.newaxis]
+        predicted_common_component = loadings @ P_hat @ G_hat_t 
+        return predicted_common_component 
+    
+    
+        
---
+++ models/FinancialReturns/NIRVAR/backtest.py
@@ -0,0 +1,96 @@
+""" 
+Script to do backtesting. Outputs csv files containing predictions hyperparameters.txt 
+NOTE: It is assumed that the backtest_design input file is clean: no NA values and has shape (T,N) 
+"""
+
+#!/usr/bin/env python3 
+# USAGE: ./backtest.py <DESIGN_MATRIX>.csv backtesting_config.yaml num_factors.csv 
+
+import numpy as np
+import sys
+import yaml 
+from fnirvar.modeling.train import FactorAdjustment 
+import os
+from numpy.random import default_rng
+
+with open(sys.argv[2], "r") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+
+###### CONFIG PARAMETERS ###### 
+SEED = config["SEED"]
+r = config["num_factors"]
+lF = config["factor_lags"]
+n_backtest_days_tot = config['n_backtest_days']
+first_prediction_day = config['first_prediction_day']
+lookback_window = config['lookback_window']
+varying_factors = config['varying_factors'] 
+save_loadings = config['save_loadings'] 
+save_factors = config['save_factors'] 
+save_predictions = config['save_predictions']
+do_NIRVAR_estimation = config['do_NIRVAR_estimation']
+NIRVAR_embedding_method = config['NIRVAR_embedding_method'] 
+use_HPC = config['use_HPC'] 
+Q = config['Q']
+
+###### ENVIRONMENT VARIABLES ######  
+if use_HPC:
+    PBS_ARRAY_INDEX = int(os.environ['PBS_ARRAY_INDEX'])
+    NUM_ARRAY_INDICES = int(os.environ['NUM_ARRAY_INDICES'])
+else:
+    PBS_ARRAY_INDEX = 1
+    NUM_ARRAY_INDICES = 1 
+
+# Re-define n_backtest_days to be total number of backtesting days divided by the number of array indices 
+n_backtest_days = int(n_backtest_days_tot/NUM_ARRAY_INDICES)
+
+# Get a list of days to do backtesting on
+days_to_backtest = [int(first_prediction_day + i + (n_backtest_days)*(PBS_ARRAY_INDEX-1)) for i in range(n_backtest_days)]
+print(f"Days to backtest: {days_to_backtest}")
+
+random_state = default_rng(seed=SEED)
+
+###### READ IN DATA ######
+Xs = np.genfromtxt(sys.argv[1], delimiter=',')
+T = Xs.shape[0]
+N_times_Q = Xs.shape[1]
+N = N_times_Q/Q
+if N != int(N):
+    print("ERROR:Input is not a whole number")
+N = int(N)
+
+Xs = np.reshape(Xs,(T,N,Q),order='F')
+Xs = Xs[:,:,1] #prCLCL 
+
+###### READ IN FACTORS ######
+if varying_factors:
+    factor_csv = np.genfromtxt(sys.argv[3], delimiter=',')
+
+###### BACKTESTING ###### 
+if do_NIRVAR_estimation:
+    pass
+else:
+    predictions = np.zeros((n_backtest_days, N)) 
+    for i, day in enumerate(days_to_backtest):
+        print(f"Day {day}") 
+        X = Xs[day-lookback_window:day+1, :] # day is the day on which you predict tomorrow's returns from 
+        if varying_factors:
+            current_r = factor_csv[i]
+        else:
+            current_r = r
+        model = FactorAdjustment(X, current_r, lF)
+        predictions[i, :] = model.predict_common_component()[:,0] 
+
+###### OUTPUT TO FILES ###### 
+if save_predictions:
+    np.savetxt(f"predictions_{PBS_ARRAY_INDEX}.csv", predictions, delimiter=',') 
+
+f = open("backtesting_hyp.txt", "w")
+f.write("{\n")
+for k in config.keys():
+    f.write("'{}':'{}'\n".format(k, config[k]))
+f.write("}")
+f.close()
+
+
+
+
