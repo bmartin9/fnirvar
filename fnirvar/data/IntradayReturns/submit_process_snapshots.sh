435e7305667a7d81743357ac3fc1a6558f4345f4
Brendan Martin <b.t.martin@protonmail.com>
2025-06-17 15:54:54 +0100

    add submit script for process snapshots

--- fnirvar/data/IntradayReturns/submit_process_snapshots.sh
+++ fnirvar/data/IntradayReturns/submit_process_snapshots.sh
@@ -1,9 +1,13 @@
 #PBS -N backtesting 
-#PBS -l walltime=03:30:00 
-#PBS -l select=1:ncpus=1:mem=8gb
+#PBS -l walltime=00:30:00 
+#PBS -l select=1:ncpus=173:mem=8gb
+#PBS -J 1-173
+
 
 module load anaconda3/personal
 source activate fnirvar
 
 cd $PBS_O_WORKDIR
-python process_snapshot.py --all
+MONTH=$(ls snapshots | sort | sed -n "$((PBS_ARRAY_INDEX+1))p")
+python process_snapshot.py --snapshot_dir snapshots/$MONTH
+
